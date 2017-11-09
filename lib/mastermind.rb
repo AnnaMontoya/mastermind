07dfd25ca717f6d7bc8a957ae83584f6548cf662
AnnaMontoya <anna.l.montoya@gmail.com>
2017-11-08 19:36:50 -0700

    initialize input

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,9 +1,7 @@
 class Mastermind
 
-  def run_game
-    self.game_intro
-    self.interpret_input
-    # puts gets.chomp
+  def initialize
+    @input = ""
   end
 
   def game_intro
@@ -12,32 +10,46 @@ class Mastermind
     puts ">"
   end
 
+  def call
+    puts self.game_intro
+    until finished?
+      self.interpret_input
+    end
+  end
+
   def interpret_input
-    input = gets.chomp
-    if input == "q" || input == "quit"
+    @input = gets.chomp
+    if finished?
       exit
-    elsif input == "p" || input == "play"
+    elsif @input == "p" || @input == "play"
       puts "I have generated a beginner sequence with four elements made up of: (r)ed,
       (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
       What's your guess?"
-      input == gets.chomp
-    elsif input == "i" || input == "instructions"
+    elsif @input == "i" || @input == "instructions"
       puts "Guess the secret sequence of 4 colors! Once you make your guess, we'll tell you
       how many you've guessed correctly and how many are in the correct position. Guess
       until you've idetified the correct sequence."
       puts "Are you ready to (p)lay, or (q)uit?"
-      input = gets.chomp
     else
-      run_game
+      puts "game"
     end
   end
+
+  def finished?
+    @input == "q" || @input == "quit"
+  end
+
 
 
 
 end
 
 
-
+#
+#
+# end
+#
 mastermind = Mastermind.new
-mastermind.run_game
-mastermind.interpret_input
+mastermind.call
+# mastermind.run_game
+# mastermind.interpret_input
